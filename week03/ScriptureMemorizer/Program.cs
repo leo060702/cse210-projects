93cce05e06ebf6cc24d265349ea1d098b212ff90
leo060702 <zzhong@byupathway.edu>
2025-05-20 00:40:31 -0700

    Initial commit of Scripture Memorizer program

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,142 @@
+// Scripture Memorizer Program - Week 03 Project
+// Name: Zhili Zhong
+// Date: 05/20/2025
+// Purpose: Help users memorize scripture by hiding words gradually using encapsulation
+
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Reference reference = new Reference("Song of Solomon", 6, 3);
+        Scripture scripture = new Scripture(reference, "I am my beloved's, and my beloved is mine: he feedeth among the lilies.");
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+
+            if (scripture.AllWordsHidden())
+            {
+                Console.WriteLine("\nAll words are hidden. Program ended.");
+                break;
+            }
+
+            Console.WriteLine("\nPress Enter to hide words or type 'quit' to exit.");
+            string input = Console.ReadLine();
+
+            if (input.Trim().ToLower() == "quit")
+            {
+                Console.WriteLine("Program exited by user.");
+                break;
+            }
+
+            scripture.HideRandomWords();
+        }
+    }
+}
+
+class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+    }
+
+    public override string ToString()
+    {
+        return $"{_book} {_chapter}:{_verse}";
+    }
+}
+
+class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    public string GetDisplayText()
+    {
+        return _isHidden ? new string('_', _text.Length) : _text;
+    }
+}
+
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random = new Random();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        string[] parts = text.Split(' ');
+        foreach (string word in parts)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords()
+    {
+        int wordsToHide = 3; // Number of words to hide per iteration
+        int hiddenCount = 0;
+
+        while (hiddenCount < wordsToHide)
+        {
+            int index = _random.Next(_words.Count);
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+                hiddenCount++;
+            }
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        List<string> displayWords = new List<string>();
+        foreach (Word word in _words)
+        {
+            displayWords.Add(word.GetDisplayText());
+        }
+        return $"{_reference.ToString()}\n" + string.Join(" ", displayWords);
+    }
+
+    public bool AllWordsHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
